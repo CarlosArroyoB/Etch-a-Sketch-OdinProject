8bbce2195059b163515c07d4461f28005126bd23
CarlosArroyoB <carlos_01_98@hotmail.com>
2024-05-08 12:57:50 -0500

    Adding buttons functionality

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const containerDivs = document.querySelector(".containerDivs");
 const range = document.querySelector("#inputRange");
 const buttonSize = document.querySelector(".button");
 const gridSize = document.querySelector("#grid-size");
+const buttonErrase = document.querySelector(".button2");
 
 let defaultSize = 16;
+let doClick = false;
+
 const createDivs = (size) => {
     let proportions = 600/size;
     let totalSquares = size*size;
@@ -14,18 +17,24 @@ const createDivs = (size) => {
     square.style.height = `${proportions}px`;
     square.style.border = "0.5px solid #000";
     square.classList.add("square");
-    square.addEventListener("mouseover", (e) => {
-      square.style.backgroundColor = "black";
+    square.addEventListener("mouseover", () => {
+      if(doClick){
+        square.style.backgroundColor = "white";
+      } else{
+        square.style.backgroundColor = "black";
+      }
+      
     });
     containerDivs.appendChild(square);
   } 
   containerDivs.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
+  return range.value;
 }
 createDivs(defaultSize);
 const changeSize = () =>{
   
   gridSize.textContent = `Grid size: ${range.value}x${range.value}`
-  createDivs(range.value);
+  createDivs(range.value,"black");
 }
 const popWindowSize  =() =>{
   let size = parseInt(prompt("Please enter a number bettwen 10 - 100: "));
@@ -34,7 +43,13 @@ const popWindowSize  =() =>{
     return popWindowSize();
   }
   gridSize.textContent = `Grid size: ${size}x${size}`
-  createDivs(size);
+  createDivs(size,"black");
 }
+
 buttonSize.addEventListener("click",popWindowSize)
 range.addEventListener("input",changeSize);
+buttonErrase.addEventListener("click",() =>{
+  doClick = !doClick;
+  console.log(doClick);
+  buttonErrase.classList.toggle('pressed');
+});
